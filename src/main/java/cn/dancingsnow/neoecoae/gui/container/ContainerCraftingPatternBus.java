6bd757a5eb0c70b30fde2a285c97fb510c77a292
Yang120231 <hnhjyang@163.com>
2026-07-17 17:28:58 +0800

    fix: 修正样板总线格式检查

--- src/main/java/cn/dancingsnow/neoecoae/gui/container/ContainerCraftingPatternBus.java
+++ src/main/java/cn/dancingsnow/neoecoae/gui/container/ContainerCraftingPatternBus.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ICrafting;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
 import cn.dancingsnow.neoecoae.tile.TileCraftingPatternBus;
