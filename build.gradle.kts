af300e17d6110116538d902ceecf2f9784b5b16b
Yang120231 <hnhjyang@163.com>
2026-07-23 23:56:59 +0800

    发布7.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.gtnewhorizons.gtnhconvention")
 }
 
-version = "7.2.1.2"
+version = "7.2.2"
 
 fun gtnhDevJar(artifactId: String, version: String) =
     "com.github.GTNewHorizons:$artifactId:$version:dev"
